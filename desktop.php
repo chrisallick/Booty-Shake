446bb3130124133871eee87e4e23fb7d774dff00
Chris Allick <chrisallick@gmail.com>
2014-08-28 10:58:27 -0400

    initial commit

---
+++ desktop.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Booty Shake</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+        <meta property="og:title" content="Train your tongue with Lick." />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="http://lickthisapp.com/img/sharethis.png" />
+        <meta property="og:url" content="http://lickthisapp.com" />
+        <meta property="og:description" content="Give your lady what she really wants for Valentine’s Day. Train your tongue with Lick. http://lickthisapp.com" />
+
+        <meta name="twitter:card" value="summary">
+        <meta name="twitter:creator" value="@chrisallick">
+        <meta name="twitter:url" content="http://lickthisapp.com">
+        <meta name="twitter:domain" content="http://lickthisapp.com">
+        <meta name="twitter:title" content="Lick.">
+        <meta name="twitter:description" content="Give your lady what she really wants for Valentine’s Day. Train your tongue with Lick.">
+        <meta name="twitter:image" content="http://lickthisapp.com/img/sharethis.png">
+        
+        <link href='./css/reset.css' media='all' rel='stylesheet' type='text/css' />
+        <link href='./css/main.css' media='all' rel='stylesheet' type='text/css' />
+
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-48081507-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+    </head>
+    <body>
+        <!--
+            Chris Allick
+            Pablo Rochat
+
+            http://clubsexytime.com/
+            
+            8==D ~~ :)
+        -->
+        <div id="wrapper">
+            <img src="./img/desktop.gif" />
+        </div>
+    </body>
+</html>
